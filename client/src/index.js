164a78c50fb1164f74e02a320513528c3802dcac
Muhammad Hammad <heyhammadhere@gmail.com>
2022-11-07 15:48:49 +0500

    Basic Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot as Root } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 Root(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
