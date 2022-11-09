95f4639e7309f0e75ceb7f8c5af0b4ddba24f6ae
Haider Ali <72439082+heyhaiderhere@users.noreply.github.com>
2022-11-09 04:50:48 -0800

    fixes

--- server/controllers/youtube.js
+++ server/controllers/youtube.js
@@ -1,19 +1,10 @@
 const { google } = require("googleapis");
 
-const CLIENT_ID = process.env.client_id;
-const CLIENT_SECRET = process.env.client_secret;
-const REDIRECT_URL = process.env.redirect_uri;
-
-const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URL
-);
+const oAuth2Client = new google.auth.OAuth2();
 
 const getChannelData = (req, res) => {
   const tokens = req.body.tokens;
   oAuth2Client.setCredentials(tokens);
-  console.log(tokens);
   const youtubeAnalytics = google.youtubeAnalytics({
     version: "v2",
     auth: oAuth2Client,
