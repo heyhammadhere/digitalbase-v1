d2886643e408c6e9a23f2e2e3a2a7e9ff1240ffa
Muhammad Hammad <heyhammadhere@gmail.com>
2022-11-13 01:16:16 +0500

    Remove Branch main

--- client/src/constants.js
+++
@@ -1,9 +0,0 @@
-export const SCOPES = [
-  "https://www.googleapis.com/auth/youtube",
-  "https://www.googleapis.com/auth/youtube.readonly",
-  "https://www.googleapis.com/auth/youtubepartner",
-  "https://www.googleapis.com/auth/yt-analytics-monetary.readonly",
-  "https://www.googleapis.com/auth/yt-analytics.readonly",
-  "https://www.googleapis.com/auth/userinfo.email",
-  "https://www.googleapis.com/auth/userinfo.profile",
-];
