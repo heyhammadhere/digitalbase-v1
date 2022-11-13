fd62ba8c37b2301d34055676b1fa5405768fb3a5
Haider Ali <72439082+heyhaiderhere@users.noreply.github.com>
2022-11-14 03:17:06 +0500

    ERRORS

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const path = require("path");
 const express = require("express");
 const app = express();
+const path = require("path");
 const cors = require("cors");
 const youtubeRouter = require("./routes/youtubeRoutes");
 const dotenv = require("dotenv").config();
@@ -12,9 +12,13 @@ const { BASE_URL } = require("./constants");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "build")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.use(`${BASE_URL}/youtube`, youtubeRouter);
 app.use(routeNotFound);
 app.use(errorHandler);
+
 app.listen(5500);
