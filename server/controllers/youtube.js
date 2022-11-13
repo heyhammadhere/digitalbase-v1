c263f2b0c87ba6a5c8f3f132572b85667f6ceb43
Haider Ali <72439082+heyhaiderhere@users.noreply.github.com>
2022-11-13 16:10:12 +0500

    Error Middlewares

--- server/controllers/youtube.js
+++ server/controllers/youtube.js
@@ -40,7 +40,42 @@ const topVideo = async (req, res) => {
       maxResults: 50,
       mine: true,
     });
-    res.send("hello");
+
+    const playList = await populerVideos.playlistItems.list({
+      part: "snippet",
+      playlistId:
+        channelDetails.data.items[0].contentDetails.relatedPlaylists.uploads,
+      maxResults: 50,
+    });
+
+    // const finalData = playList.data.items.splice(0, 3).map((video) => {
+    //   return {
+    //     videoId: video.snippet.resourceId.videoId,
+    //     channelTitle: video.snippet.channelTitle,
+    //     videoTitle: video.snippet.title,
+    //   };
+    // });
+
+    const videosArray = playList.data.items.map((video) => {
+      return video.snippet.resourceId.videoId;
+    });
+
+    const response = await populerVideos.videos.list({
+      part: "snippet,statistics",
+      maxResults: 50,
+      id: videosArray.join(","),
+      order: "viewCount",
+    });
+
+    const sortedData = response.data.items.sort((a, b) => {
+      if (Number(a.statistics.viewCount) > Number(b.statistics.viewCount))
+        return -1;
+      if (Number(a.statistics.viewCount) < Number(b.statistics.viewCount))
+        return 1;
+      return 0;
+    });
+
+    res.send(sortedData);
   } catch (error) {
     console.log(error);
   }
@@ -83,8 +118,66 @@ const latestVideos = async (req, res) => {
   }
 };
 
+const bestThumbnails = async (req, res) => {
+  try {
+    const tokens = req.body.tokens;
+    oAuth2Client.setCredentials(tokens);
+
+    const populerVideos = google.youtube({
+      version: "v3",
+      auth: oAuth2Client,
+    });
+
+    const channelDetails = await populerVideos.channels.list({
+      part: "snippet,statistics,contentDetails",
+      maxResults: 50,
+      mine: true,
+    });
+
+    const playList = await populerVideos.playlistItems.list({
+      part: "snippet",
+      playlistId:
+        channelDetails.data.items[0].contentDetails.relatedPlaylists.uploads,
+      maxResults: 50,
+    });
+
+    const videosArray = playList.data.items.map((video) => {
+      return video.snippet.resourceId.videoId;
+    });
+
+    const response = await populerVideos.videos.list({
+      part: "snippet,statistics",
+      maxResults: 50,
+      id: videosArray.join(","),
+      order: "viewCount",
+    });
+
+    const sortedData = response.data.items.sort((a, b) => {
+      if (Number(a.statistics.viewCount) > Number(b.statistics.viewCount))
+        return -1;
+      if (Number(a.statistics.viewCount) < Number(b.statistics.viewCount))
+        return 1;
+      return 0;
+    });
+
+    const thumbnailsData = sortedData.map((video) => {
+      return {
+        views: video.statistics.viewCount,
+        likes: video.statistics.likeCount,
+        title: video.snippet.title,
+        thumbnails: video.snippet.thumbnails.high.url,
+      };
+    });
+
+    res.send(thumbnailsData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 module.exports = {
   getChannelData,
   topVideo,
   latestVideos,
+  bestThumbnails,
 };
--- server/index.js
+++ server/index.js
@@ -4,38 +4,15 @@ const app = express();
 const cors = require("cors");
 const youtubeRouter = require("./routes/youtubeRoutes");
 const dotenv = require("dotenv").config();
-const { google } = require("googleapis");
-const oAuth2Client = new google.auth.OAuth2();
-const { METRICS } = require("./constants");
-
+const {
+  routeNotFound,
+  errorHandler,
+} = require("./middlewares/errorMiddleware");
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.post("/", async (request, response) => {
-//   try {
-//     const { token } = request.body;
-//     oAuth2Client.setCredentials(token);
-//     const analytics = google.youtubeAnalytics({
-//       version: "v2",
-//       auth: oAuth2Client,
-//     });
-//     const reports = await analytics.reports.query({
-//       ids: "channel==MINE",
-//       startDate: "2012-01-01",
-//       endDate: "2022-01-01",
-//       dimensions: "month",
-//       metrics: METRICS.join(),
-//     });
-//     response.status(200).json(reports);
-//   } catch ({ message }) {
-//     response.status(500).json({
-//       error: "Internal Server Error",
-//       message: message,
-//     });
-//   }
-// });
-
 app.use("/youtube", youtubeRouter);
-
+app.use(routeNotFound);
+app.use(errorHandler);
 app.listen(5500);
